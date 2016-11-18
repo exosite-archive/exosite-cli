c56b4b9c5f9e8683bbccd150f25ad346199e9cd0
Freda Lai <fredalai@exosite.com>
2016-11-18 10:00:10 +0800

    Version 0.26

--- exosite.py
+++ exosite.py
@@ -44,7 +44,7 @@ pool = ThreadPool(4)
 
 SECRET_FILE = '.Solutionfile.secret'
 CONFIG_FILE = 'Solutionfile.json'
-VERSION = '0.25'
+VERSION = '0.26'
 LATEST_FORMAT_VERSION = '0.3'
 PREVIOUS_FORMAT_VERSION = '0.2'
 
